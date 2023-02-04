d35f9489211c67fdcad025cb4f67a2791a68b002
Steven Krenn <stevekrenn@gmail.com>
2023-02-04 12:08:14 -0600

    fixed footer padding, relative positioning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,25 +4,20 @@ import { AiFillPhone, AiTwotoneMail } from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <div className="container background-color"
-            style={{
-                backgroundColor: '61A5FF',
-            }}>
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <div className="col-md-4 d-flex align-items-center">
-                    <a href="/" className="mb-3 me-2 mb-md-0  text-decoration-none lh-1">
-                        <svg className="bi" width={30} height={24}><use xlinkHref="#bootstrap" /></svg>
-                    </a>
-                    <span className="mb-3 mb-md-0 ">© 2022 Steven Krenn</span>
-                </div>
-                <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-                    <li className="ms-3"><a className="" href="https://github.com/Krenbot"><BsGithub /></a></li>
-                    <li className="ms-3"><a className="" href="https://www.linkedin.com/in/krenbot/"><BsLinkedin /></a></li>
-                    <li className="ms-3"><a className="" href="tel:262-707-8842"><AiFillPhone /></a></li>
-                    <li className="ms-3"><a className="" href="mailto: 'stevekrenn@gmail.com"><AiTwotoneMail /></a></li>
-                </ul>
-            </footer>
-        </div>
+        <footer className="d-flex flex-wrap justify-content-between align-items-center border-top">
+
+            <div className="col-md-4 d-flex align-items-center">
+                <span className="mb-3 mb-md-0 ">© 2022 Steven Krenn</span>
+            </div>
+
+            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li className="ms-3"><a className="" href="https://github.com/Krenbot"><BsGithub /></a></li>
+                <li className="ms-3"><a className="" href="https://www.linkedin.com/in/krenbot/"><BsLinkedin /></a></li>
+                <li className="ms-3"><a className="" href="tel:262-707-8842"><AiFillPhone /></a></li>
+                <li className="ms-3"><a className="" href="mailto: 'stevekrenn@gmail.com"><AiTwotoneMail /></a></li>
+            </ul>
+
+        </footer>
     );
 }
 
