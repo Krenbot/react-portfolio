b9011c07fc2bf53611b5b070e104c78963abbc1a
Steven Krenn <stevekrenn@gmail.com>
2023-02-06 10:53:25 -0600

    skeleton contact form working

--- src/pages/Contactme.jsx
+++ src/pages/Contactme.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import { Container, Row, Col } from "react-bootstrap"
-import { validateEmail } from '../utils/helpers';
 
-function Contactme() {
+function Contact() {
     const [formState, setFormState] = useState({
         name: '',
         email: '',
@@ -12,83 +10,56 @@ function Contactme() {
     const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!errorMessage) {
-            console.log('Submit Form', formState);
-        }
-    };
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     if (!errorMessage) {
+    //         console.log('Submit Form', formState);
+    //     }
+    // };
 
-    const onFormUpdate = (e) => {
-        if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-            if (!isValid) {
-                setErrorMessage('Your email is invalid.');
-            } else {
-                setErrorMessage('');
-            }
-        } else {
-            if (!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`);
-            } else {
-                setErrorMessage('');
-            }
-        }
-        if (!errorMessage) {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-            console.log('Handle Form', formState);
-        }
-    };
+    // const onFormUpdate = (e) => {
+    //     if (e.target.name === 'email') {
+    //         const isValid = validateEmail(e.target.value);
+    //         if (!isValid) {
+    //             setErrorMessage('Your email is invalid.');
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     } else {
+    //         if (!e.target.value.length) {
+    //             setErrorMessage(`${e.target.name} is required.`);
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     }
+    //     if (!errorMessage) {
+    //         setFormState({ ...formState, [e.target.name]: e.target.value });
+    //         console.log('Handle Form', formState);
+    //     }
+    // };
 
     return (
-        <section className="contact" id="connect">
-            <Container>
-                <Row className="align-items-center">
-                    <Col size={12} md={6}>
-
-                        <div>
-                            <h2>Get In Touch</h2>
-                            <form onSubmit={handleSubmit}>
-                                <Row>
-                                    <Col size={12} sm={6} className="px-1">
-                                        <input
-                                            type="text"
-                                            placeholder="Name"
-                                            name="name"
-                                            defaultValue={name}
-                                            onBlur={onFormUpdate} />
-                                    </Col>
-                                    <Col size={12} sm={6} className="px-1">
-                                        <input
-                                            type="email"
-                                            placeholder="Email Address"
-                                            name="email"
-                                            defaultValue={email}
-                                            onBlur={onFormUpdate} />
-                                    </Col>
-                                    <Col size={12} className="px-1">
-                                        <textarea
-                                            rows="6"
-                                            placeholder="Message"
-                                            name="message"
-                                            defaultValue={message}
-                                            onBlur={onFormUpdate}>
-                                        </textarea>
-                                        {errorMessage && (
-                                            <div>
-                                                <p className="error-text">{errorMessage}</p>
-                                            </div>
-                                        )}
-                                        <button type="submit"><span>Send</span></button>
-                                    </Col>
-                                </Row>
-                            </form>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
+        <div className="container pt-5">
+            <div className="mb-3">
+                <label className="form-label"></label>
+                Name:
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Your Name Here..." />
+            </div>
+            <div className="mb-3">
+                <label className="form-label"></label>
+                Email:
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="email@email.com..." />
+            </div>
+            <div className="mb-3">
+                <label className="form-label"></label>
+                Message:
+                <textarea className="form-control" placeholder="Questions, comments, concerns..." id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button className="text-center btn btn-outline-light btn-floating m-1">
+                <i className="bi bi-envelope-paper-heart-fill"> Submit</i>
+            </button>
+        </div>
     );
 }
 
-export default Contactme;
+export default Contact;
