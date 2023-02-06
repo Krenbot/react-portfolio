300b6b984207cbc77709b85c0292174d8c02ef18
Steven Krenn <stevekrenn@gmail.com>
2023-02-06 10:43:03 -0600

    installed bootstrap in package json, email helper function applied, test format contact form

--- src/pages/Contactme.jsx
+++ src/pages/Contactme.jsx
@@ -1,42 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Container, Row, Col } from "react-bootstrap"
+import { validateEmail } from '../utils/helpers';
 
-const Contactme = () => {
+function Contactme() {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            console.log('Submit Form', formState);
+        }
+    };
+
+    const onFormUpdate = (e) => {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+            console.log('Handle Form', formState);
+        }
+    };
+
     return (
-        <section>
-            <br />
-            <form id="contact-form" action='mailto: stevekrenn@gmail.com' method="post" enctype="text/plain">
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder='Your name here'
+        <section className="contact" id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col size={12} md={6}>
 
-                    />
-                </div>
-                <br />
-                <div>
-                    <label htmlFor="email">Email address:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder='user@test.com'
-                    />
-                </div>
-                <br />
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea
-                        name="message"
-                        rows="5"
-                        placeholder='Enter message...'
-                    />
-                </div>
-
-                <button type="submit">Submit</button>
-            </form>
+                        <div>
+                            <h2>Get In Touch</h2>
+                            <form onSubmit={handleSubmit}>
+                                <Row>
+                                    <Col size={12} sm={6} className="px-1">
+                                        <input
+                                            type="text"
+                                            placeholder="Name"
+                                            name="name"
+                                            defaultValue={name}
+                                            onBlur={onFormUpdate} />
+                                    </Col>
+                                    <Col size={12} sm={6} className="px-1">
+                                        <input
+                                            type="email"
+                                            placeholder="Email Address"
+                                            name="email"
+                                            defaultValue={email}
+                                            onBlur={onFormUpdate} />
+                                    </Col>
+                                    <Col size={12} className="px-1">
+                                        <textarea
+                                            rows="6"
+                                            placeholder="Message"
+                                            name="message"
+                                            defaultValue={message}
+                                            onBlur={onFormUpdate}>
+                                        </textarea>
+                                        {errorMessage && (
+                                            <div>
+                                                <p className="error-text">{errorMessage}</p>
+                                            </div>
+                                        )}
+                                        <button type="submit"><span>Send</span></button>
+                                    </Col>
+                                </Row>
+                            </form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </section>
-    )
+    );
 }
 
-export default Contactme
+export default Contactme;
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,22 +4,19 @@ const Resume = () => {
     return (
         <div className="container my-3 py-3">
 
-            <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing"><img id="resume-image" src='/images/resume.jpg' alt="Steven Krenn resume" />
-
+            <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing">
+                <img id="resume-image" src='/images/resume.jpg' alt="Steven Krenn resume" />
             </a>
 
             <div id="resume-link" className="m-2">
-                <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing"><i class="bi bi-link-45deg"></i>Resume Link</a>
+                <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing"><i class="bi bi-link-45deg"></i>Google Drive PDF</a>
             </div>
 
-            <div className="m-2">
-                <a href="./pdf/krennResume.pdf" download="krennResume"><i className="bi bi-cloud-arrow-down-fill"></i>DOWNLOAD BUTTON PLACEHOLDER</a>
+            <div>
+                <a href="./pdf/krennResume.pdf" download="krennResume"><i className="bi bi-cloud-arrow-down-fill"></i></a>
             </div>
 
-
-
-
-            {/* <h6>Technical Skills</h6>
+            <h6>Technical Skills</h6>
             <p>
                 Languages - ADD IMAGES
             </p>
@@ -28,7 +25,7 @@ const Resume = () => {
             </p>
             <p>
                 Tools - ADD IMAGES
-            </p> */}
+            </p>
 
         </div>
     )
