83e61c00ad17e4fb5c67a70cdd151d9de4e4ee6e
Steve Krenn <stevekrenn@gmail.com>
2023-02-16 10:48:41 -0600

    added dev icons to profile

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Resume = () => {
     return (
+
         <div className="container my-3 py-3">
+
+
+
 
             <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing">
                 <img id="resume-image" src='/images/resume.jpg' alt="Steven Krenn resume" />
             </a>
 
             <div id="resume-link" className="m-2">
-                <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing"><i class="bi bi-link-45deg"></i>Google Drive PDF</a>
+                <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing"><i className="bi bi-link-45deg"></i>Google Drive PDF</a>
             </div>
 
             <div>
@@ -17,12 +23,20 @@ const Resume = () => {
             </div>
 
             <h6>Technical Skills</h6>
+
+            <FontAwesomeIcon icon="fa-brands fa-square-js" />
+
+
             <p>
-                Languages - ADD IMAGES
+                Languages
+                <i className="fa-brands fa-square-js"></i>
             </p>
             <p>
+
                 Applications - ADD IMAGES
+
             </p>
+
             <p>
                 Tools - ADD IMAGES
             </p>
@@ -34,3 +48,4 @@ const Resume = () => {
 export default Resume
 
 // src={resume}
+
