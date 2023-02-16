1f201791a924f274324358859fb707e831164e44
Steven Krenn <stevekrenn@gmail.com>
2023-02-16 16:18:54 -0600

    fixed headshot image link

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Aboutme = () => {
     return (
         <div id='about-me' className="container my-3 flex-md-column flex-lg-row">
-            <img id='headshot' alt='Steven Krenn headshot' src='/react-portfolio/self.jpg' />
+            <img id='headshot' alt='Steven Krenn headshot' src='./images/self.jpg' />
             <div id='aboutMe'>
                 <h2>Full Stack Web Developer</h2>
                 <p>Description</p>
