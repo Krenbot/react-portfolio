8f74eba0107a2b8c8310e5e60f1213070673d025
Steven Krenn <stevekrenn@gmail.com>
2023-02-16 13:13:14 -0600

    added footer padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { BsLinkedin, BsGithub } from 'react-icons/bs'
 import { AiFillPhone, AiTwotoneMail } from 'react-icons/ai'
+import { right } from '@popperjs/core'
 
 const Footer = () => {
     return (
         <footer className="d-flex flex-wrap justify-content-between align-items-center border-top">
 
             <div className="col-md-4 d-flex align-items-center">
-                <span className="mb-3 mb-md-0 ">© 2022 Steven Krenn</span>
+                <span className="mb-3 mb-md-0 " style={{ paddingLeft: '1em' }}>© 2022 Steven Krenn</span>
             </div>
 
-            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex" style={{ paddingRight: '1em', display: "flex", flexDirection: "row" }}>
                 <li className="ms-3"><a className="" href="https://github.com/Krenbot"><BsGithub /></a></li>
                 <li className="ms-3"><a className="" href="https://www.linkedin.com/in/krenbot/"><BsLinkedin /></a></li>
                 <li className="ms-3"><a className="" href="tel:262-707-8842"><AiFillPhone /></a></li>
                 <li className="ms-3"><a className="" href="mailto: 'stevekrenn@gmail.com"><AiTwotoneMail /></a></li>
             </ul>
 
-        </footer>
+        </footer >
     );
 }
 
 export default Footer
+
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,23 +4,24 @@ const Resume = () => {
     return (
 
         <div className="container my-3 py-3">
+            <div>
+                <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" />
 
+                <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original-wordmark.svg" />
 
+                <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
 
-
-            <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing">
-                <img id="resume-image" src='/images/resume.jpg' alt="Steven Krenn resume" />
-            </a>
-
-            <div id="resume-link" className="m-2">
-                <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing"><i className="bi bi-link-45deg"></i>Google Drive PDF</a>
+                <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
             </div>
 
-            <div>
+            <div >
+                <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing">
+                    <img id="resume-image" src='/images/resume.jpg' alt="Steven Krenn resume" />
+                </a>
                 <a href="./pdf/krennResume.pdf" download="krennResume"><i className="bi bi-cloud-arrow-down-fill"></i></a>
             </div>
 
-            <h6>Technical Skills</h6>
+
             <div style={{ display: "flex", flexDirection: "row" }}>
                 <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
 
@@ -30,19 +31,8 @@ const Resume = () => {
 
                 <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
 
-
-                <div>
-                    <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" />
-
-                    <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original-wordmark.svg" />
-
-                    <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
-
-                    <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
-                </div>
-
             </div>
-        </div>
+        </div >
     )
 }
 
