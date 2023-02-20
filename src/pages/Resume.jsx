bc12fb83d94b3968c04191e2a8a60c7895b3bde6
Steve Krenn <stevekrenn@gmail.com>
2023-02-19 18:53:07 -0600

    porfolio stylings

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,34 +3,43 @@ import React from 'react'
 const Resume = () => {
     return (
 
-        <div className="container my-3 py-3">
-            <div>
+        <div className="container my-3 py-3" style={{ display: "flex", justifyContent: "center", flexDirection: "column" }} >
+            <div style={{ display: "flex", justifyContent: "space-between", width: '15em' }}>
                 <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" />
 
                 <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original-wordmark.svg" />
-
-                <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
-
-                <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
-            </div>
-
-            <div >
-                <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing">
-                    <img id="resume-image" src='/images/resume.jpg' alt="Steven Krenn resume" />
-                </a>
-                <a href="./pdf/krennResume.pdf" download="krennResume"><i className="bi bi-cloud-arrow-down-fill"></i></a>
             </div>
 
 
-            <div style={{ display: "flex", flexDirection: "row" }}>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: 'center' }}>
+                <div style={{ display: "flex", justifyContent: "space-between", flexDirection: 'column', height: '25em' }}>
+                    <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+
+                    <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+                </div>
+
+                <div className='imageContainer'>
+                    <a href="https://docs.google.com/document/d/1dkkZ2JOBx--CuWsuis0NWvJXpENTEEb-qLa2GhX7nxA/edit?usp=sharing">
+                        <img id="resume-image" src='/images/resume.jpg' alt="Steven Krenn resume" />
+                    </a>
+
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: 'center', marginTop: '.5em' }}>
+                        <a className='btn btn-primary' href="./pdf/krennResume.pdf" download="krennResume">CLICK HERE TO DOWNLOAD RESUME</a>
+                    </div>
+                </div>
+
+                <div style={{ display: "flex", justifyContent: "space-between", flexDirection: 'column', height: '25em' }}>
+                    <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+
+                    <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+                </div>
+            </div>
+
+
+            <div style={{ display: "flex", justifyContent: "space-between", width: '15em' }}>
                 <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
 
                 <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-
-                <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-
-                <img height='100' width='100' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-
             </div>
         </div >
     )
@@ -40,3 +49,4 @@ export default Resume
 
 // src={resume}
 
+// style={{ display: "flex", justifyContent: "center" }}
